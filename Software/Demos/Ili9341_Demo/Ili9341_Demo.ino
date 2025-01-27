d459c5d9426978be2ba6ad6bcaef945cae3d2074
GeekLanyu <silence2021silence@163.com>
2025-01-27 18:53:51 +0800

    Update Ili9341_Demo.ino
    更新内容：新增设置屏幕方向的函数、显示16位图像的函数

--- Software/Demos/Ili9341_Demo/Ili9341_Demo.ino
+++ Software/Demos/Ili9341_Demo/Ili9341_Demo.ino
@@ -1,13 +1,23 @@
 #include <stdint.h>
 #include <Arduino.h>
 #include "ili9341.h"
+#include "pic1.h"
+#include "pic2.h"
 
 void setup() {
   LCD_Init();
 }
 
 void loop() {
+  LCD_Set_Display_Dir(0);
   LCD_Clear(0xF800);
   LCD_Clear(0x07E0);
   LCD_Clear(0x001F);
+  LCD_ShowPic_16bit(0, 0, 240, 320, gImage_pic1);
+  delay(1000);
+  LCD_Set_Display_Dir(1);
+  LCD_Clear(0xF800);
+  LCD_Clear(0x07E0);
+  LCD_Clear(0x001F);
+  LCD_ShowPic_16bit(0, 0, 320, 240, gImage_pic2);
 }
