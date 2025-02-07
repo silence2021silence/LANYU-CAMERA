f50dad8410c59b16533c9485217112367d7cc292
GeekLanyu <silence2021silence@163.com>
2025-02-07 23:40:51 +0800

    Update camera.cpp
    更新内容：增加拍照保存到TF卡功能

--- Software/Demos/Camera_Demo/camera.cpp
+++ Software/Demos/Camera_Demo/camera.cpp
@@ -1,9 +1,12 @@
+#include "esp32-hal-gpio.h"
 #include <stdint.h>
 #include <Arduino.h>
 #include "camera.h"
+#include "sd.h"
 
 camera_fb_t * fb = NULL;
 esp_err_t res = ESP_OK;
+
 int CONTRAST = 0;
 int BRIGHTNESS = 0;
 int SATURATION = 0;
@@ -33,14 +36,16 @@ void Camera_Init(void)
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;  //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+  config.xclk_freq_hz = 10000000;  //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
   config.pixel_format = PIXFORMAT_JPEG;  //YUV422,GRAYSCALE,RGB565,JPEG
+  // config.pixel_format = PIXFORMAT_RGB565;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   
   // 是否识别到PSRAM
   if(psramFound()){
     Serial.printf("Camera init with PSRAM!\n");
-    config.frame_size = FRAMESIZE_HVGA; // 480*320
+    // config.frame_size = FRAMESIZE_HVGA; // 480*320
+    config.frame_size = FRAMESIZE_QVGA; // 320*240
     config.jpeg_quality = 10;  // JPEG输出的质量。0-63越低，质量越好
     config.fb_count = 2; // 要分配的帧缓冲区的数目。如果大于1，则每帧采集（双倍速度）
     config.grab_mode = CAMERA_GRAB_LATEST;
@@ -73,6 +78,7 @@ void Camera_Init(void)
 
 }
 
+// JPEG通过串口发送
 void Jpeg2Serial(void)
 {
   fb = esp_camera_fb_get();  // 获取帧缓冲区指针
@@ -92,19 +98,25 @@ void Jpeg2Serial(void)
   }
 }
 
+// 初始化按键
+void Key_Init(void)
+{ 
+  // 初始化引脚 设置为上拉输入
+  pinMode(CONTRAST_KEY, INPUT_PULLUP);
+  pinMode(BRIGHTNESS_KEY, INPUT_PULLUP);
+  pinMode(SATURATION_KEY, INPUT_PULLUP);
+  pinMode(SHARPNESS_KEY, INPUT_PULLUP);
+  pinMode(TAKE_PICTURE_KEY, INPUT_PULLUP);
+}
+
+// 按键调节摄像头参数
 void Camera_Adjuster(void)
 {
-  // 初始化引脚 设置为上拉输入
-  pinMode(SET_CONTRAST_KEY, INPUT_PULLUP);
-  pinMode(SET_BRIGHTNESS_KEY, INPUT_PULLUP);
-  pinMode(SET_SATURATION_KEY, INPUT_PULLUP);
-  pinMode(SET_SHARPNESS_KEY, INPUT_PULLUP);
-
   // 调节对比度
-  if(digitalRead(SET_CONTRAST_KEY)==LOW){
+  if(digitalRead(CONTRAST_KEY)==LOW){  // 按键按下
     delay(20);  // 按键消抖
-    if(digitalRead(SET_CONTRAST_KEY)==LOW){
-      while(digitalRead(SET_CONTRAST_KEY)==LOW);
+    if(digitalRead(CONTRAST_KEY)==LOW){
+      while(digitalRead(CONTRAST_KEY)==LOW);  // 等待按键松开
       CONTRAST++;
       if (CONTRAST > 2) CONTRAST = -2;  // 范围在-2和2之间循环
       sensor_t * s = esp_camera_sensor_get();
@@ -113,10 +125,10 @@ void Camera_Adjuster(void)
   }
 
   // 调节亮度
-  if(digitalRead(SET_BRIGHTNESS_KEY)==LOW){
+  if(digitalRead(BRIGHTNESS_KEY)==LOW){
     delay(20);
-    if(digitalRead(SET_BRIGHTNESS_KEY)==LOW){
-      while(digitalRead(SET_BRIGHTNESS_KEY)==LOW);
+    if(digitalRead(BRIGHTNESS_KEY)==LOW){
+      while(digitalRead(BRIGHTNESS_KEY)==LOW);
       BRIGHTNESS++;
       if (BRIGHTNESS > 2) BRIGHTNESS = -2;
       sensor_t * s = esp_camera_sensor_get();
@@ -125,10 +137,10 @@ void Camera_Adjuster(void)
   }
 
   // 调节饱和度
-  if(digitalRead(SET_SATURATION_KEY)==LOW){
+  if(digitalRead(SATURATION_KEY)==LOW){
     delay(20);
-    if(digitalRead(SET_SATURATION_KEY)==LOW){
-      while(digitalRead(SET_SATURATION_KEY)==LOW);
+    if(digitalRead(SATURATION_KEY)==LOW){
+      while(digitalRead(SATURATION_KEY)==LOW);
       SATURATION++;
       if (SATURATION > 2) SATURATION = -2;
       sensor_t * s = esp_camera_sensor_get();
@@ -137,14 +149,45 @@ void Camera_Adjuster(void)
   }
 
   // 调节清晰度
-  if(digitalRead(SET_SHARPNESS_KEY)==LOW){
+  if(digitalRead(SHARPNESS_KEY)==LOW){
     delay(20);
-    if(digitalRead(SET_SHARPNESS_KEY)==LOW){
-      while(digitalRead(SET_SHARPNESS_KEY)==LOW);
+    if(digitalRead(SHARPNESS_KEY)==LOW){
+      while(digitalRead(SHARPNESS_KEY)==LOW);
       SHARPNESS++;
       if (SHARPNESS > 2) SHARPNESS = -2;
       sensor_t * s = esp_camera_sensor_get();
       s->set_sharpness(s, 1);
+    }
+  }
+}
+
+// 拍照按键
+void Take_Picture_Key(void)
+{
+  if(digitalRead(TAKE_PICTURE_KEY)==LOW){
+    delay(20);
+    if(digitalRead(TAKE_PICTURE_KEY)==LOW){
+      while(digitalRead(TAKE_PICTURE_KEY)==LOW);
+      fb = esp_camera_fb_get();
+      if (!fb) 
+      {
+        Serial.println("Camera capture failed");
+        res = ESP_FAIL;
+      }
+      if(res == ESP_OK)
+      {
+        int photo_index = readFileNum(SD_MMC, "/");  // 获取文件名数字索引
+        if(photo_index!=-1)
+        {
+          String path = "/" + String(photo_index) +".jpg";  // 组合路径和文件名
+          writejpg(SD_MMC, path.c_str(), fb->buf, fb->len);  // 写入TF卡
+        }
+      }
+      if(fb)
+      {
+        esp_camera_fb_return(fb);
+        fb = NULL;
+      }
     }
   }
 }
