58fdafe2c96a16b1433826ec4e4a822a354b89a9
GeekLanyu <silence2021silence@163.com>
2025-02-07 23:39:52 +0800

    Update Camera_Demo.ino
    更新内容：增加拍照保存到TF卡功能

--- Software/Demos/Camera_Demo/Camera_Demo.ino
+++ Software/Demos/Camera_Demo/Camera_Demo.ino
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <Arduino.h>
 #include "camera.h"
+#include "sd.h"
 
 void setup() 
 {
@@ -8,10 +9,13 @@ void setup()
   Serial.begin(921600);
   Serial.setDebugOutput(false);
   Camera_Init();
+  Key_Init();
+  sdmmcInit();
 }
 
 void loop() 
 {
   Jpeg2Serial();
   Camera_Adjuster();
+  Take_Picture_Key();
 }
